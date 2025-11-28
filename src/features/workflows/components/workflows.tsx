65417121d7e84dce468762631deff8219e33ebcc
Vansh1379 <vanshkalra1379@gmail.com>
2025-11-28 20:42:48 +0530

    added changes

--- src/features/workflows/components/workflows.tsx
+++ src/features/workflows/components/workflows.tsx
@@ -1,8 +1,40 @@
-// "use client";
-// import { useSuspenseWorkflows } from "../hooks/use-workflows";
+"use client";
+import { EntityContainer, Entityheader } from "@/components/entity-components";
+import { useSuspenseWorkflows } from "../hooks/use-workflows";
 
-// export const WorkflowsList = () => {
-//   const workflows = useSuspenseWorkflows();
+export const WorkflowsList = () => {
+  const workflows = useSuspenseWorkflows();
 
-//   return <p>{JSON.stringify(workflows.data, null, 2)}</p>;
-// };
+  return <p>{JSON.stringify(workflows.data, null, 2)}</p>;
+};
+
+export const WorkflowHeader = ({ disabled }: { disabled?: boolean }) => {
+  return (
+    <>
+      <Entityheader
+        title="Workflows"
+        description="Create and Manage your workflows"
+        onNew={() => {}}
+        newButtonLabel="New Workflow"
+        disabled={disabled}
+        isCreating={false}
+      />
+    </>
+  );
+};
+
+export const WorkflowsContainer = ({
+  children,
+}: {
+  children: React.ReactNode;
+}) => {
+  return (
+    <EntityContainer
+      header={<WorkflowHeader />}
+      search={<></>}
+      pagination={<></>}
+    >
+      {children}
+    </EntityContainer>
+  );
+};
--- src/features/workflows/hooks/use-workflows.ts
+++ src/features/workflows/hooks/use-workflows.ts
@@ -1,8 +1,11 @@
 import { useTRPC } from "@/trpc/client";
 import { useSuspenseQuery } from "@tanstack/react-query";
 
+//hook to fetch all the workflows using susspense
 export const useSuspenseWorkflows = () => {
   const trpc = useTRPC();
 
   return useSuspenseQuery(trpc.workflows.getMany.queryOptions());
 };
+
+// hook to create a new workflow
